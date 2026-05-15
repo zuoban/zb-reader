192787850296651879cdb246c0a0e3b4352acbd4
zuoban <1061669148@qq.com>
2026-05-16 01:10:25 +0800

    feat(next.config.ts): 添加图片质量配置

--- next.config.ts
+++ next.config.ts
@@ -20,6 +20,7 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [],
     formats: ["image/webp", "image/avif"],
+    qualities: [75, 85],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 60 * 60 * 24 * 30,
